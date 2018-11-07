417c4c6d89dca3ffaf2e6450b92281ab1dac9a0d
Julie Munn <jmunn302@gmail.com>
2018-11-07 00:42:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,24 +14,3 @@ function renderBooks(json) {
   })
 }
 
-function fetchBooks() {
-  //write fetch request to the Game of Thrones API
-  fetch('https://anapioficeandfire.com/api/houses')
-  .then(resp => resp.json())
-  .then(json => console.log(json));
-}
-
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-}
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
